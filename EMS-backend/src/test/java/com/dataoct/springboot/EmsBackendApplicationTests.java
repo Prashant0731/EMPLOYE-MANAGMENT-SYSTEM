745c49259a45ee9b18ed82cd2235ec75d237efe4
Prashant <prashantchauhan1@hotmail.com>
2021-03-28 16:19:46 +0530

    First commit

---
+++ EMS-backend/src/test/java/com/dataoct/springboot/EmsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dataoct.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
